113931912b469fb0aa55fa0ecd82e1f61f8958ae
Conor Carey <careyc@union.edu>
2016-09-10 18:04:55 -0700

    set up auth page

--- server/authentication.js
+++ server/authentication.js
@@ -1,1 +1,15 @@
-import Passport from 'passport-twitter';
+var passport = require('passport')
+  , TwitterStrategy = require('passport-twitter').Strategy;
+
+passport.use(new TwitterStrategy({
+    consumerKey: '1FcM1FOspmD28EQO2qEDTD4He',
+    consumerSecret: 'eyA2FhpWWzWmSodJdov0KIAA7ovXWn0u8XehAUEQrqeG3icwpq',
+    callbackURL: "http://www.example.com/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    User.findOrCreate(..., function(err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
