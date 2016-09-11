fdc6612ce69844e1c970f1cbba5fe56d50ddfcc2
Reid Greer <jreid.greer@gmail.com>
2016-09-10 22:20:19 -0700

    Standardize logging

--- server/controllers/haven.js
+++ server/controllers/haven.js
@@ -1,20 +1,21 @@
+const logger = require('../utils/logger');
 module.exports = (user, img) => {
   const havenondemand = require('havenondemand');
   const client = new havenondemand.HODClient(process.env.HAVEN);
 
   client.post('detectfaces', {url: img}, (err1, resp1, body1) => {
     if (err1) {
-      console.log('Error occured during facial detection', err);
+      logger.error(`Error occured during facial detection: ${err}`);
     }
-    console.log(`Face Detected for ${user.name}'s photo`);
+    logger.info(`Face Detected for ${user.name}'s photo`);
     // Check if there was a face found
     if(body1.face.length){
       client.post('recognizeimages', {url: img}, (err, resp, body) => {
-        body.object.forEach(logo => console.log(`${logo.name} Logo Detected for ${user.name}'s photo`))
-        console.log('Haven Image Body:', body);
+        body.object.forEach(logo => logger.info(`${logo.name} Logo Detected for ${user.name}'s photo`))
+        // console.log('Haven Image Body:', body);
       });
     } else {
-      console.log('Haven: No face found.');
+      logger.error('Haven: No face found.');
     }
   });
 }
