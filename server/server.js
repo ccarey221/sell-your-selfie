228d3e5acfd3ed499c5f8f195614e10f3a436b1b
Mark Tyneway <tyneway12@gmail.com>
2016-09-10 17:35:56 -0700

    Add middleware logger

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const logger = require('./utils/logger');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(logger.requestLogger);
 require('./middleware')(app);
 require('./routes')(app);
-
 app.use('/', express.static(`${__dirname}/../client`));
+app.use(logger.errorLogger);
 
 app.listen(PORT, () => console.log(`App listening on ${PORT}`));
